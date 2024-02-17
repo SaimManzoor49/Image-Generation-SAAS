90fbf04352dbb229e448b4d3bdb4aead5b269821
unknown <saummanzoor49@gmail.com>
2024-02-17 23:27:28 +0500

    sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,8 +1,15 @@
+'use client'
+import { navLinks } from '@/constants'
+import { SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = () => {
+
+    const pathname = usePathname()
+
   return (
     <aside className='sidebar'>
         <div className="flex size-full flex-col gap-4">
@@ -17,6 +24,36 @@ const Sidebar = () => {
                 height={28}
                 />
             </Link>
+
+            <nav className='sidebar-nav'>
+            <SignedIn>
+                <ul className='sidebar-nav_elements'>
+                    {navLinks.map((link)=>{
+                        const isActive = link.route === pathname
+                        return (
+                            <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white":"text-gray-700"}`}>
+                                <Link 
+                                className='sidebar-link'
+                                href={link.route}
+                                >
+                                    <Image 
+                                    src={link.icon}
+                                    alt='logo'
+                                    width={24}
+                                    height={24}
+                                    className={`${isActive && 'brightness-200'}`}
+                                    />
+                                    {
+                                        link.label
+                                    }
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </SignedIn>
+            </nav>
+
         </div>
     </aside>
   )
