1c9b0cf142306738a19c9ee6e1a9b2836d7c8cf3
unknown <saummanzoor49@gmail.com>
2024-02-20 00:15:51 +0500

    Image-model

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,25 +1,29 @@
-import mongoose,{Mongoose} from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn:Mongoose | null;
-    promise:Promise<Mongoose>|null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
-let cached:MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose;
 
-
-if(!cached){
-    cached = (global as any).mongoose = {
-        conn:null,promise:null
-    }
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
-
-
-export const connectDB = async()=>{
-    if(cached.conn)return cached.conn
-    if(!MONGODB_URL) throw new Error("Missing MONGODB_ULR")
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL,{dbName:"imginify",bufferCommands:false})
-cached.conn = await cached.promise
-}
+export const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_ULR");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "imginify",
+      bufferCommands: false,
+    });
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
