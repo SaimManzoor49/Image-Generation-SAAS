84ef5d75833ec211dcb12cf2036b63c55cddae12
unknown <saummanzoor49@gmail.com>
2024-02-21 23:29:36 +0500

    Transaction-type

--- lib/database/models/transaction.model.ts
+++ lib/database/models/transaction.model.ts
@@ -1,44 +1,41 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
+
+export interface ITransaction extends Document{
+  createdAt: Date;
+  stripeId: string;
+  amount: number;
+  plan?: string;
+  credits?: number;
+  buyer: Schema.Types.ObjectId
+}
 
 
-const UserSchema = new Schema({
-    clerkId:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    photo:{
-        type:String,
-        required:true,
-    },
-    firstName:{
-        type:String,
-    },
-    lastName:{
-        type:String,
-    },
-    planId:{
-        type:Number,
-        default:1
-    },
-    creditBalance:{
-        type:Number,
-        default:10
-    },
+const TransactionSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  plan: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
-})
+const Transaction = models?.Transaction || model("Transaction", TransactionSchema);
 
-const User = models?.User || model("User",UserSchema)
-
-
-export default User
+export default Transaction;
