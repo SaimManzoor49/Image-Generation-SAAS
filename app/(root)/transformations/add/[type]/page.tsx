f0f9aa893c7630ff8cefe562aa45ebcad68ae546
unknown <saummanzoor49@gmail.com>
2024-02-24 23:54:21 +0500

    custom-form-field

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,14 +2,34 @@ import Header from '@/components/shared/Header'
 import React from 'react'
 
 import {transformationTypes} from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { auth } from '@clerk/nextjs'
+import { getUserById } from '@/lib/actions/user.action'
+import { redirect } from 'next/navigation'
 
 
-const AddTransformationsTypePage = ({params:{type}}:SearchParamProps) => {
+const AddTransformationsTypePage = async({params:{type}}:SearchParamProps) => {
+
+  const {userId} = auth();
+
+  if(!userId) redirect('sign-in')
+
+  const user =await getUserById(userId)
+  
+  const transformation = transformationTypes[type]
   return (
+    <>
     <Header 
-    title="Transformation Title"
-    subTitle="Transformation SubTitle"
+    title={transformation.title}
+    subTitle={transformation.subTitle}
     />
+    <TransformationForm 
+    action="Add"
+    userId={user._id}
+    type={transformation.type as TransformationTypeKey}
+    creditBalance={user.creditBalance}
+     />
+    </>
   )
 }
 
